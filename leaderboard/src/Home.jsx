255ca8c4b6d78ad25de07385c828acffc7a04347
cbh778899 <cbh778899@outlook.com>
2024-10-29 16:12:55 +1100

    update styles
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

--- leaderboard/src/Home.jsx
+++ leaderboard/src/Home.jsx
@@ -16,18 +16,27 @@ export default function Home() {
 
     return (
         <div className="leader-board">
-            <div className="title-bar">
-                <img src="/RMIT.jpg" className="icon" draggable={false} />
-                <h1>RMIT Hackthon Leaderboard</h1>
+            <div className="fixed-title">
+                <div className="title-bar">
+                    <img src="/RMIT.jpg" className="icon" draggable={false} />
+                    <h1>RMIT Hackthon Leaderboard</h1>
+                </div>
+                <div className="column-title-container">
+                    <div className="column-title">
+                        <div className="name">Participant Name</div>
+                        <div className="score">Score</div>
+                        <div className="group">Group Name</div>
+                    </div>
+                </div>
             </div>
             <div className="participants">
             {
                 leaders.map(({name, score, group}, index)=>{
                     return (
                         <div className="participant" key={`leader-${index}`}>
-                            <div className="name">{name}</div>
-                            <div className="score">{score}</div>
-                            <div className="group">{group}</div>
+                            <div className="name" title={name}>{name}</div>
+                            <div className="score" title={score}>{score}</div>
+                            <div className="group" title={group}>{group}</div>
                         </div>
                     )
                 })
